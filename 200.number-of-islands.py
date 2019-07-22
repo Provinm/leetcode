feec691174f30a884f54328a2f40a34f670b9713
Provinm <zhouxin576@gmail.com>
2019-07-22 23:05:30 +0800

    update

--- 200.number-of-islands.py
+++ 200.number-of-islands.py
@@ -44,7 +44,7 @@
 #
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def dfs(i,j):w
+        def dfs(i,j):
 
             if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != "1":
                 return 
